45f70540245dda84b8d5f33127c30868d055bf17
allanteone <allanteone@gmail.com>
2023-02-24 15:51:45 +0200

    hw-29

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
 "use strict";
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(() => console.log(i));
-// }
-let num;
-num = 12;
-let str = "string";
-str = "lmn";
-let style;
-//style = "abc"; error
-let color;
-function f1(n1, n2) {
-    return n1 + n2;
-}
-//f1(1, "1"); error
-function f2(array) {
-    return array.reduce((res, cur) => res + cur);
-}
-function stringNumberAction(strNum) {
-    let res;
-    if (typeof strNum == "string") {
-        res = strNum.length;
-    }
-    else {
-        res = strNum * 2;
-    }
+function shiftRound(str, shift = 1) {
+    let arr = Array.from(str);
+    let res = arr.map(letter => shiftLetter(letter, shift)).join("");
     return res;
 }
-console.log(stringNumberAction("10"));
-let ar;
-ar = [1, 2, 3];
-ar = ["a", "b", "c"];
-let ar1;
-ar1 = [1, "a", 2, "b"];
-let someColor;
-let someComparator;
-someComparator = (n1, n2) => n1 - n2;
-const person = { id: 11, name: "Vasya", age: 23 };
-//hw-29
-function shiftRound(str, shift = 1) {
-    return "";
-    //adds shift  to ascii codes of lower case letters 
-    //if the shidted code will be greater then code of "z"
-    //going by the circle from "a"
-}
 function unshiftRound(str, shift = 1) {
-    return "";
+    let arr = Array.from(str);
+    let res = arr.map(letter => shiftLetter(letter, -shift)).join("");
+    return res;
+}
+function shiftLetter(letter, shift) {
+    const A_CODE = "a".charCodeAt(0);
+    const Z_CODE = "z".charCodeAt(0);
+    const LETTERS_COUNT = Z_CODE - A_CODE + 1;
+    let code = letter.charCodeAt(0);
+    if (code >= A_CODE && code <= Z_CODE) {
+        code += shift % LETTERS_COUNT;
+        if (code > Z_CODE) {
+            code -= LETTERS_COUNT;
+        }
+        else if (code < A_CODE) {
+            code += LETTERS_COUNT;
+        }
+    }
+    return String.fromCharCode(code);
 }
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,72 +1,32 @@
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(() => console.log(i));
-// }
-
-let num: number;
-num = 12;
-
-let str: string = "string";
-str = "lmn";
-
-let style: "backgroundColor" | "color";
-//style = "abc"; error
-
-let color: "red" | "blue" | "green";
-
-function f1(n1: number, n2: number): number {
-    return n1 + n2;
-}
-//f1(1, "1"); error
-
-function f2(array: number[]): number {
-    return array.reduce((res, cur) => res + cur)
-}
-
-function stringNumberAction(strNum: string | number) : number {
-    let res: number;
-    if (typeof strNum == "string") {
-        res = strNum.length;
-    } else {
-        res = strNum * 2;
-    }
+function shiftRound(str: string,  shift: number = 1): string {
+    let arr: string[] = Array.from(str);
+    let res: string = arr.map(letter => shiftLetter(letter, shift)).join("");
+    
     return res;
 }
 
-console.log(stringNumberAction("10"));
-
-let ar: number[] | string[];
-ar = [1, 2, 3];
-ar = ["a", "b", "c"];
-
-let ar1: (number | string)[];
-ar1 = [1, "a", 2, "b"];
-
-type Color = "red"| "green"| "blue" | "grey" | "yellow";
-let someColor: Color;
-
-type Comparator = (num1: number, num2: number) => number;
-
-let someComparator: Comparator;
-someComparator = (n1, n2) => n1 - n2;
-
-type Person = {
-    id: number;
-    name: string;
-    age?: number; //not required
-}
-
-const person:Person = {id: 11, name: "Vasya", age: 23};
-
-//hw-29
-
-function shiftRound(str: string,  shift: number = 1): string {
-    return "";
-
-    //adds shift  to ascii codes of lower case letters 
-    //if the shidted code will be greater then code of "z"
-    //going by the circle from "a"
-}
-
 function unshiftRound(str: string,  shift: number = 1): string {
-    return "";
+    let arr: string[] = Array.from(str);
+    let res: string = arr.map(letter => shiftLetter(letter, -shift)).join("");
+    
+    return res;
+}
+
+function shiftLetter(letter: string, shift: number) : string {
+    const A_CODE: number = "a".charCodeAt(0);
+    const Z_CODE: number = "z".charCodeAt(0);
+    const LETTERS_COUNT: number = Z_CODE - A_CODE + 1;
+
+    let code: number = letter.charCodeAt(0);
+        
+        if (code >= A_CODE && code <= Z_CODE) {
+            code += shift % LETTERS_COUNT;
+
+            if (code > Z_CODE) {
+                code -= LETTERS_COUNT;
+            } else if (code < A_CODE) {
+                code += LETTERS_COUNT;
+            }
+        }
+    return String.fromCharCode(code);
 }
